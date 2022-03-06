c86d2ee0de41fc1d02bc7ddeaa456452b8b6a0c5
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-03-05 23:08:17 -0600

    fixed author return to map through each authors cookbook array and display information

--- src/authors/authors.js
+++ src/authors/authors.js
@@ -1,86 +1,97 @@
 import { useState } from "react";
 import apiUrl from "./../apiURL";
 const Authors = () => {
-    const [authors, setAuthors] = useState([]);
-    const [author, setAuthor] = useState({
-      firstName: "",
-      lastName: "",
-      cookbooks: [],
-    });
-  
-    const handleClickAuthor = () => {
-      fetch(apiUrl +`authors`)
-        .then((response) => response.json())
-        .then((data) => setAuthors(data));
-    };
-  
-    const handleChangeAuthor = (event) => {
-      event.persist();
-      setAuthor((prevAuthor) => {
-        const editedAuthor = {
-          ...prevAuthor,
-          [event.target.name]: event.target.value,
-        };
-        return editedAuthor;
-      });
-    };
-  
-    const handleSubmitAuthor = (event) => {
-      event.preventDefault();
-      console.log(author);
-      fetch(apiUrl +`authors`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        body: JSON.stringify(author),
-      })
-        .then(() => fetch(apiUrl + 'authors'))
-        .then((response) => response.json())
-        .then((data) => setAuthors(data))
-        .then(() => setAuthor({ firstName: "", lastName: "", cookbooks: [] }));
-    };
-    const authorList = authors.map((author) => (
-        <div key={author._id} className="author-card">
-          <div>
-            Author: {author.firstName} {author.lastName}
-          </div>
-          <div>Cookbooks: {/*{bookSearch(author.cookbooks)}*/}</div>
-        </div>
-      ));
-return(
-<>
-<form onSubmit={handleSubmitAuthor} className="new-author-form">
-<input
-  onChange={handleChangeAuthor}
-  value={author.firstName}
-  name="firstName"
-  placeholder="firstName"
-/>
-<input
-  onChange={handleChangeAuthor}
-  value={author.lastName}
-  name="lastName"
-  placeholder="lastName"
-/>
-{/* non functioning, will shutdown server */}
-<input
-  onChange={handleChangeAuthor}
-  value={author.cookbooks[{}]}
-  name="cookbooks"
-  placeholder="cookbooks"
-/>
-<button type="Submit">Add author</button>
-</form>
-{/* cook book list */}
-{/* author list */}
-<section className="author-card-list">
-<button className="author-button" onClick={handleClickAuthor}>
-  View authors
-</button>
-{authorList}
-</section>
-</>)
-}
+  const [authors, setAuthors] = useState([]);
+  const [author, setAuthor] = useState({
+    firstName: "",
+    lastName: "",
+    cookbooks: [],
+  });
 
-export default Authors
+  const handleClickAuthor = () => {
+    fetch(apiUrl + `authors`)
+      .then((response) => response.json())
+      .then((data) => setAuthors(data));
+  };
+
+  const handleChangeAuthor = (event) => {
+    event.persist();
+    setAuthor((prevAuthor) => {
+      const editedAuthor = {
+        ...prevAuthor,
+        [event.target.name]: event.target.value,
+      };
+      return editedAuthor;
+    });
+  };
+
+  const handleSubmitAuthor = (event) => {
+    event.preventDefault();
+    console.log(author);
+    fetch(apiUrl + `authors`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(author),
+    })
+      .then(() => fetch(apiUrl + "authors"))
+      .then((response) => response.json())
+      .then((data) => setAuthors(data))
+      .then(() => setAuthor({ firstName: "", lastName: "", cookbooks: [] }));
+  };
+  const authorList = authors.map((author) => (
+    <div key={author._id} className="author-card">
+      <div>
+        Author: {author.firstName} {author.lastName}
+      </div>
+      <div>
+        Cookbooks:{" "}
+        {author.cookbooks.map((items) => {
+          return(
+          <div>
+            <h4>{items.title}</h4>
+            <h5>{items.yearPublished}</h5>
+          </div>
+          )
+        })}
+      </div>
+    </div>
+  ));
+  return (
+    <>
+      <form onSubmit={handleSubmitAuthor} className="new-author-form">
+        <input
+          onChange={handleChangeAuthor}
+          value={author.firstName}
+          name="firstName"
+          placeholder="firstName"
+        />
+        <input
+          onChange={handleChangeAuthor}
+          value={author.lastName}
+          name="lastName"
+          placeholder="lastName"
+        />
+        {/* non functioning, will shutdown server */}
+        <input
+          onChange={handleChangeAuthor}
+          value={author.cookbooks[{}]}
+          name="cookbooks"
+          placeholder="cookbooks"
+        />
+        <button type="Submit">Add author</button>
+      </form>
+      {/* cook book list */}
+      {/* author list */}
+      <section className="author-card-list">
+        <button className="author-button" onClick={handleClickAuthor}>
+          View authors
+        </button>
+        {authorList}
+      </section>
+    </>
+  );
+};
+
+export default Authors;
